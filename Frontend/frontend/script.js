f8b1b0cccd28da7df7021f1e3ddc8bbfcf65bb35
AlanahCoolman <alanah.emily.coleman@gmail.com>
2025-01-03 15:50:57 +0100

    Update script.js

--- Frontend/frontend/script.js
+++ Frontend/frontend/script.js
@@ -91,3 +91,20 @@ document.getElementById("fetchCoordinates").addEventListener("click", async () =
 // Kör plats- och väderhämtning vid inloggning (sidladdning)
 window.addEventListener("load", fetchLocationAndWeather);
 document.getElementById("getLocation").addEventListener("click", fetchLocationAndWeather);
+
+document.getElementById("play-button").addEventListener("click", async () => {
+    await fetch("/play-playlist", { method: "PUT" });
+});
+
+document.getElementById("pause-button").addEventListener("click", async () => {
+    await fetch("/pause", { method: "PUT" });
+});
+
+document.getElementById("prev-button").addEventListener("click", async () => {
+    await fetch("/previous", { method: "POST" });
+});
+
+document.getElementById("next-button").addEventListener("click", async () => {
+    await fetch("/next", { method: "POST" });
+});
+
